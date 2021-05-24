2e506b0a958f9a34037d65aff8470935c8006167
mario <mario.pellegrino1989@gmail.com>
2021-05-24 11:10:22 +0200

    Initial commit

---
+++ src/main/java/com/mpellegrino/amazon_bot/AmazonBotApplication.java
@@ -0,0 +1,13 @@
+package com.mpellegrino.amazon_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpellegrino/amazon_bot/AmazonBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpellegrino.amazon_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
